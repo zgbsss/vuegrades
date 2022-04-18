1ddb33b3f5b570a46c48f22c7a5aa03e8892836d
zgbsss <99218597+zgbsss@users.noreply.github.com>
2022-04-18 09:25:20 -0300

    Add files via upload

---
+++ poxaa/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import HeaDer from "@components/HeaDer.vue";
+export default {
+  components: { MenuLateral HeaDer },
+};
+</script>
+
+<template>
+  <MenuLateral />
+  <header class="header">Cabeçalho</header>
+  <main class="content">Principal</main>
+  <footer class="footer">Rodapé</footer>
+</template>
+
+<style></style>
---
+++ poxaa/src/components/MenuLateral.vue
@@ -0,0 +1,16 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <aside class="sidenav">
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+      <li>Item 4</li>
+      <li>Item 5</li>
+      <li>Item 6</li>
+    </ul>
+  </aside>
+</template>
